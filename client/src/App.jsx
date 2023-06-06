7f651828957289f3289c3a345a46ca749682081e
Mike Veilleux <115963834+esfdcict@users.noreply.github.com>
2023-06-06 13:07:05 +0800

    Implemented login, logout, cookie and JWT

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,115 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
   const [message, setMessage] = useState("No Message");
+  const [loginName, setLoginName] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
 
   async function login() {
+    setErrorMsg("");
     const response = await axios({
       method: "POST",
       url: "http://localhost:3000/login",
+      withCredentials: true,
       data: {
-        name: "kai",
+        name: loginName,
       },
     });
-
+    if (response.status === 204) {
+      setErrorMsg(`${loginName} is not in out database.`);
+    } else {
+      setErrorMsg("");
+    }
     const data = response.data;
-    console.log(data);
-
-    setMessage(data.name);
+    setMessage(data);
   }
 
-  async function test() {
+  async function logout() {
+    setErrorMsg("");
+    const response = await axios({
+      method: "POST",
+      url: "http://localhost:3000/logout",
+      withCredentials: true,
+    });
+    const data = response.data;
+    setMessage(data);
+  }
+
+  async function TestAuthRequest() {
+    setErrorMsg("");
     const response = await axios({
       method: "POST",
       url: "http://localhost:3000/test",
       withCredentials: true,
     });
-
-    const data = response.data;
-    console.log(response.data);
-
-    setMessage(data);
+    if (response.status == 204) {
+      setErrorMsg(`Authenticated request FAILED!`);
+    } else {
+      setErrorMsg(`Authenticated request SUCCESSFUL!`);
+    }
   }
+
+  useEffect(() => {
+    setErrorMsg("");
+    setLoginName(message);
+  }, []);
 
   return (
     <>
-      <div>
-        <button onClick={() => login()}>Login</button>
-        <button onClick={() => test()}>Test</button>
+      <h2>Cookies and JWT Test bed</h2>
+      <div style={{ paddingBottom: 15 }}>
+        <input
+          type="text"
+          value={loginName}
+          placeholder="Enter your name..."
+          onChange={(e) => setLoginName(e.target.value)}
+        />
       </div>
-      <br />
+      <div>
+        <button
+          disabled={loginName === "" ? true : false}
+          onClick={() => login()}
+        >
+          Login
+        </button>
+      </div>
+      <div>
+        <button onClick={() => TestAuthRequest()}>
+          Test Authenticated Request
+        </button>
+      </div>
+      <div>
+        <button
+          disabled={Object.keys(message).length > 0 ? false : true}
+          onClick={() => logout()}
+        >
+          Logout
+        </button>
+      </div>
 
-      <div>{message}</div>
+      <h3>Console logs...</h3>
+      <div className="logContainer">
+        {errorMsg == "" && (
+          <>
+            {Object.keys(message).length > 0 ? (
+              <>
+                <div>
+                  <strong>Logged in as:</strong>
+                </div>
+                <div>{`id:   ${message.id}`}</div>
+                <div>{`name:   ${message.name}`}</div>
+                <div>{`title:   ${message.title}`}</div>
+              </>
+            ) : (
+              <div>Cookie has been invalidated</div>
+            )}
+          </>
+        )}
+        {errorMsg !== "" && <div>{errorMsg}</div>}
+      </div>
     </>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,98 +1,117 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 const app = express();
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", req.header("origin"));
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
+  res.header("Access-Control-Allow-Origin", req.header("origin"));
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
 });
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(
-    cors({
-        credentials: true,
-        origin: true,
-    })
+  cors({
+    credentials: true,
+    // origin: ["http://127.0.0.1:3000"],
+    origin: true,
+  })
 );
 
 const users = [
-    {
-        id: "asdfsadfsd654asefs54",
-        name: "mike"
-    },
-    {
-        id: "asdfsadfsd654asefs54",
-        name: "kai"
-    },
-    {
-        id: "asdfsadfsd654asefs54",
-        name: "danielle"
-    },
-    {
-        id: "asdfsadfsd654asefs54",
-        name: "eva"
-    }
-]
+  {
+    id: "123456910",
+    name: "mike",
+    title: "Software Engineer",
+  },
+  {
+    id: "abcdefg",
+    name: "kai",
+    title: "Student",
+  },
+  {
+    id: "65as4d654asd",
+    name: "danielle",
+    title: "Teacher",
+  },
+  {
+    id: "btsPower",
+    name: "eva",
+    title: "Unemployed",
+  },
+];
 
-const MySecret = "MySecret"
+const MySecret = "MySecret";
 
 app.post("/login", (req, res) => {
-    const name = req.body.name;
-    const user = users.find(x => x.name === name)
-    const userID = user.id
-    console.log(userID)
-    console.log(user.name)
+  const name = req.body.name;
+  const user = users.find((x) => x.name === name);
+  console.log(user);
 
-    if (user === null) {
-        res.status(204).send();
-    } else {
-        const token = jwt.sign(
-            { value: userID },
-            MySecret
-        );
+  if (user === undefined) {
+    res.status(204).send();
+  } else {
+    const userID = user.id;
+    console.log(userID);
+    const token = jwt.sign({ value: userID }, MySecret);
 
-        res.cookie("mvx_jwt",
-            token,
-            { httpOnly: true, sameSite: 'strict', secure: true })
-            // { sameSite: 'strict', secure: true, })
-            .send();
-    }
-})
+    res
+      .cookie("mvx_jwt", token, {
+        httpOnly: true,
+        sameSite: "None",
+        secure: true,
+        maxAge: 9 * 60 * 60 * 1000, // hours x minutes x seconds x milliseconds
+      })
+      .send(user);
+  }
+});
 
+app.post("/logout", (req, res) => {
+  const token = jwt.sign({ value: "Expired" }, MySecret);
+  console.log("Logout Signed Token: ", token);
+  res
+    .cookie("mvx_jwt", token, {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+      maxAge: 0,
+    })
+    .send({});
+});
 
 app.post("/test", CheckJWT, (req, res) => {
-    console.log("Request was authenticated")
-
-    res.send(req.user)
-})
-
+  console.log("Request was authenticated");
+  const user = users.find((x) => x.id === req.user);
+  if (user === undefined) {
+    res.status(204).send();
+  } else {
+    res.send(user);
+  }
+});
 
 function CheckJWT(req, res, next) {
-    console.log("Entering CheckJWT...")
-    const token = req.cookies.mvx_jwt;
-    console.log("🚀 ~ file: server.js:80 ~ CheckJWT ~ token:", token)
-
-    if (token == null) return res.sendStatus(401);
-
-    jwt.verify(token, MySecret, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user.value;
-        console.log(user);
-        console.log("Token is valid!");
-        next();
+  console.log("Entering CheckJWT...");
+  const token = req.cookies.mvx_jwt;
+  console.log("Encoded Token: ", token);
+  console.log("Raw Cookie: ", req.cookies);
+  if (token == null) {
+    res.status(401);
+  } else {
+    jwt.verify(token, MySecret, (err, result) => {
+      if (err) return res.sendStatus(403);
+      req.user = result.value;
+      console.log("Decoded token:", result);
+      console.log("Token is valid!");
     });
+  }
+  next();
 }
 
-
-
 app.listen(3000, () => {
-    console.log("Server running at http://localhost:3000")
-})
-
+  console.log("Server running at http://localhost:3000");
+});
